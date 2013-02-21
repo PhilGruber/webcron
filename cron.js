fbb4e20363200736cb6dc02c146456f6fe906596
Philipp Gruber <philipp.gruber@flupps.net>
2013-02-21 21:12:48 +1100

    wip

---
+++ cron.js
@@ -0,0 +1,23 @@
+
+
+function add() {
+	$("#cron").append("<tr><td>\
+				<input class='num' type='text' name='data["+linecount+"][h]' />\
+				<input class='num' type='text' name='data["+linecount+"][m]' />\
+			</td>\
+			<td><input class='num' type='text' name='data["+linecount+"][dom]' /></td>\
+			<td><input class='num' type='text' name='data["+linecount+"][mon]' /></td>\
+			<td><input class='num' type='text' name='data["+linecount+"][dow]' /></td>\
+			<td><input class='' type='text' name='data["+linecount+"][cmd]' /></td>\
+			<td>\
+				<input type='hidden' id='state-"+linecount+"' name='data["+linecount+"][state]' value='normal' />\
+				<img onclick='del("+linecount+");' src='icons/delete.png' alt='X' />\
+			</td>\
+		</tr>");
+	linecount++;
+}
+
+function del(id) {
+	$("#state-"+id).val('deleted');
+	$("#row-"+id).hide();
+}
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+error_reporting(E_ALL);
+
 $crontab = shell_exec('crontab -l');
 $lines = explode("\n", $crontab);
 
@@ -19,6 +21,7 @@ foreach ($lines as $id => $l) {
 		list($time, $cmd) = explode(' ',$l, 2);
 	else
 		list($m, $h, $dom, $mon, $dow, $cmd) = explode(' ',$l, 6);
+
 	$data[$id] = array(
 		'm' => $m,
 		'h' => $h,
@@ -30,28 +33,44 @@ foreach ($lines as $id => $l) {
 }
 
 if (isset($_POST['save'])) {
-	foreach($_POST['data'] AS $id => $d)
-		$file[$id] = "{$d['m']}\t{$d['h']}\t{$d['dom']}\t{$d['mon']}\t{$d['dow']}\t{$d['cmd']}";
+	foreach($_POST['data'] AS $id => $d) {
+		if ($d['state'] == 'deleted') {
+			unset($file[$id]);
+			echo "deleted<br />";
+		} else
+			$file[$id] = "{$d['m']}\t{$d['h']}\t{$d['dom']}\t{$d['mon']}\t{$d['dow']}\t{$d['cmd']}";
+	}
 	$output = '';
 	foreach ($file as $l)
 		$output .= "$l\n";
+	$output = preg_replace("!\n+$!", "\n", $output);
+
+	file_put_contents('/tmp/crontab.plain', print_r($file, true));
 	file_put_contents('/tmp/crontab.txt', $output);
 	exec('crontab /tmp/crontab.txt');
 	$message = 'Crontab was saved.';
 }
+	print_r($_POST);
+	print_r($data);
 ?>
 <html>
 	<head>
 		<link rel='stylesheet' type='text/css' href='cron.css' />
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="cron.js"></script>
+		<script>
+			var linecount = <?= count($file); ?>;
+		</script>
 	</head>
 	<body>
+	<div class='crontab'>
 	<?php if (isset($message)): ?>
 	<div class='notice'>
 		<?= $message ?>
 	</div>
 	<?php endif; ?>
 		<form method='POST'>
-		<table class='cron'>
+		<table id='cron'>
 			<tr>
 				<td>Time</td>
 				<td>Day</td>
@@ -61,7 +80,7 @@ if (isset($_POST['save'])) {
 				<td></td>
 			</tr>
 		<?php foreach ($data as $id => $e): ?>
-		<tr>
+		<tr id='row-<?= $id ?>'>
 			<td>
 				<input class='num' type='text' name='data[<?= $id ?>][h]' value='<?= $e['h'] ?>' />
 				<input class='num' type='text' name='data[<?= $id ?>][m]' value='<?= $e['m'] ?>' />
@@ -75,13 +94,16 @@ if (isset($_POST['save'])) {
 				<input class='' type='text' name='data[<?= $id ?>][cmd]' value='<?= $e['cmd'] ?>' />
 			</td>
 			<td>
-				<input type='hidden' name='data[<?= $id ?>][state]' value='normal' />
-				<img onclick='delete(<?= $id ?>);' src='' alt='X' />
+				<input type='hidden' id='state-<?= $id ?>' name='data[<?= $id ?>][state]' value='normal' />
+				<img onclick='del(<?= $id ?>);' src='icons/delete.png' alt='X' />
 			</td>
 		</tr>
 		<?php endforeach; ?>
 		</table>
+		<img class='addbutton' onclick='add(<?= $id ?>);' src='icons/add.png' alt='+' />
+		<br />
 		<input type='submit' value='save' name='save' />
 		</form>
+		</div>
 	</body>
 </html>
